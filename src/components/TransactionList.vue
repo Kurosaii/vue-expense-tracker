e890ea6f1b0112b3ff9fed266632e119632a4a24
Benjamin Evan Cowles <becowles93@gmail.com>
2024-06-06 18:29:23 -0400

    feat: Templatize the transaction list and add dummy data

--- src/components/TransactionList.vue
+++ src/components/TransactionList.vue
@@ -1,3 +1,28 @@
+<script setup>
+const transactions = [
+    {
+        id: 1,
+        title: 'Flower',
+        amount: -19.99,
+    },
+    {
+        id: 2,
+        title: 'Salary',
+        amount: 299.97,
+    },
+    {
+        id: 3,
+        title: 'Book',
+        amount: -10,
+    },
+    {
+        id: 4,
+        title: 'Camera',
+        amount: 150,
+    },
+];
+</script>
+
 <template>
     <h3>History</h3>
 
@@ -5,14 +30,13 @@
         id="list"
         class="list"
     >
-        <li class="minus">
-            <button class="delete-btn">x</button>
-            Cash <span>$-400</span>
-        </li>
-
-        <li class="plus">
-            <button class="delete-btn">x</button>
-            Paycheck <span>$800</span>
+        <li
+            v-for="transaction in transactions"
+            :key=transaction.id
+            :class="transaction.amount < 0 ? 'minus' : 'plus'"
+        >
+            <button class=" delete-btn">x</button>
+            {{ transaction.title }}<span>${{ transaction.amount }}</span>
         </li>
     </ul>
 </template>
